59e720472bd4b6a8ad17fd58cedabcac64f43dbe
Jędrzej Runowicz <j.runowicz@tkhtechnology.com>
2022-04-27 13:53:27 +0200

    url's names changes

--- app/main.py
+++ app/main.py
@@ -9,25 +9,25 @@ import uvicorn
 app = FastAPI()
 
 
-@app.get("/api/v1/get")
+@app.get("/api/v1/users")
 async def fetch_users():
     """localhost:8000/api/v1/get  >  fetch users from db"""
     return database.get_users()
 
 
-@app.post("/api/v1/post")
+@app.post("/api/v1/users")
 async def registger_user(user: User):
     """localhost:8000/api/v1/post   >  adding users with POST"""
     return database.join_user_data(user)
 
 
-@app.delete("/api/v1/delete/{user_id}")
+@app.delete("/api/v1/users/{user_id}")
 async def delete_user(user_id: UUID):
     """localhost:8000/api/v1/delete/{user_id}  >  delete users by user_id"""
     return database.delete_users(user_id)
 
 
-@app.put("/api/v1/put")
+@app.put("/api/v1/users/1")
 async def update_user(user_update: User):
     """localhost:8000/api/v1/put  >  update user data"""
     return database.update_users(user_update)
